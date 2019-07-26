08c6b9a3bc2c3e1888fff2549648946442f241f2
AnastasiiaBlaha <anastasiia.blaha@gmail.com>
2019-07-26 15:00:16 +0100

    API weather check different cities

--- src/interface.js
+++ src/interface.js
@@ -17,6 +17,17 @@ $(document).ready(function() {
       thermostat.resetTemperature();
       updateTemperature();
     });
+
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+    $('#current-temperature').text(data.main.temp);
+    });
+    
+    $('#current-city').change(function() {
+        var city = $('#current-city').val();
+        $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+          $('#current-temperature').text(data.main.temp)
+        })
+      })
   
     $('#psm-on').click(function() {
       thermostat.switchPowerSavingModeOn();
@@ -33,5 +44,6 @@ $(document).ready(function() {
     function updateTemperature() {
       $('#temperature').text(thermostat.temperature);
       $('#temperature').attr('class', thermostat.energyUsage());
+      
     };
   });
